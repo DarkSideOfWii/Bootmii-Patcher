14768ffa7a80ee38fbef651f7ea4a58261a25613
DarkSideOfWii <112542668+DarkSideOfWii@users.noreply.github.com>
2022-09-02 17:30:21 +0200

    Add files via upload
    The Bootmii for MacOS and Linux

---
+++ BootMiiPatcher.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd Desktop/
+mkdir Bootmii
+cd Bootmii
+git clone https://github.com/DarkSideOfWii/Bootmii-Patcher
+echo "Thanks to used the patcher, now you can install Bootmii, an essential Brick Protection for your old Wii console"
